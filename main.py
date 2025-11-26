15cda6d3aec8a80c89b1bd3fe1af5c391e3d8037
Cuong Do <101785334+dDxCg@users.noreply.github.com>
2025-11-26 20:19:17 +0700

    config: uvicorn server quick run

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ async def test():
     return {"status": "ok"}
 
 app.include_router(api_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
