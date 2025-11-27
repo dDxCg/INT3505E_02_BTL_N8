c09a72782e89b098b0a5a9d55a3ceb497a403e84
hieunguyen0801 <23020664@vnu.edu.vn>
2025-11-27 11:40:24 +0700

    Remove postgres for pull request

--- schemas/payments/__init__.py
+++ schemas/payments/__init__.py
@@ -1,8 +1,6 @@
+# schemas/payments/__init__.py
+
 from .payments import (
-    Currency,
-    PaymentMethod,
-    PaymentStatus,
-    PaymentProvider,
     Payment,
     PaymentCreate,
     PaymentUpdate,
@@ -10,8 +8,10 @@ from .payments import (
     PaymentRefund,
 )
 
-#cd C:\Users\Lenovo\INT3505E_02_BTL_N8
-#.\.venv-backend\Scripts\Activate.ps1
-#$env:DATABASE_URL="postgresql://neondb_owner:npg_QrhJyMX1p5Sq@ep-proud-heart-a1dii8dg-pooler.ap-southeast-1.aws.neon.tech/neondb"
-#$env:ENV = "local"
-#uvicorn main:app --reload
+__all__ = [
+    "Payment",
+    "PaymentCreate",
+    "PaymentUpdate",
+    "PaymentWebhookPayload",
+    "PaymentRefund",
+]
