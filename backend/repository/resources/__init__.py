8f72d252a139bc1700273b48bfb85234541804ad
Cuong Do <101785334+dDxCg@users.noreply.github.com>
2025-12-09 20:32:20 +0700

    be-feat: table status

--- backend/repository/resources/__init__.py
+++ backend/repository/resources/__init__.py
@@ -1,4 +1,4 @@
 from .Equipment import EquipmentRepository, EquipmentTypeRepository, EquipmentStatusRepository
 from .Ingredient import IngredientRepository, IngredientUnitRepository, IngredientHistoryRepository
-from .Table import TableRepository
+from .Table import TableRepository, TableStatusRepository
 from .Dish import DishRepository
